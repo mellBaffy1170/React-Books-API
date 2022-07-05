f4ff8d7da0e331901b40280ad2917690bec1ffca
mellBaffy1170 <71152648+mellBaffy1170@users.noreply.github.com>
2022-07-05 11:06:35 +0300

    refactor: extracted routes paths to constant

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import BookList from "pages/BookList/BookList";
 import BookDetails from "pages/BookDetails/BookDetails";
 import NotFound from "pages/NotFound/NotFound";
+import { ROUTES } from "constants/routes";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Navigate replace to="/book-list" />} />
-        <Route path={"/book-list"} element={<BookList />} />
-        <Route path={"/book-details"} element={<BookDetails />} />
-        <Route path={"*"} element={<NotFound />} />
+        <Route index element={<Navigate replace to={ROUTES.bookList} />} />
+        <Route path={ROUTES.bookList} element={<BookList />} />
+        <Route path={ROUTES.bookDetails} element={<BookDetails />} />
+        <Route path={ROUTES.any} element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/constants/routes.js
@@ -0,0 +1,5 @@
+export const ROUTES = {
+  bookList: "/book-list",
+  bookDetails: "/book-details",
+  any: "*",
+};
