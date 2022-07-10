3ac96f055cc7e988e884cacb2b7e19a5d0fbb525
mellBaffy1170 <71152648+mellBaffy1170@users.noreply.github.com>
2022-07-10 21:02:19 +0300

    feat: implemented API request

--- src/components/PageLayout/PageLayoutContainer.jsx
+++ src/components/PageLayout/PageLayoutContainer.jsx
@@ -1,9 +1,10 @@
 import { useFormik } from "formik";
-import { useState } from "react";
+// import { useState } from "react";
 
 import { CATEGORIES } from "constants/categories";
 import { SORT } from "constants/sort";
 import BooksListContext from "context/BooksListContext";
+import useGetBooks from "api/getBooks";
 
 import PageLayout from "./PageLayout";
 
@@ -11,10 +12,14 @@ const PageLayoutContainer = () => {
   const [defaultCategory] = CATEGORIES;
   const [defaultSort] = SORT;
 
-  const [queryValues, setQueryValues] = useState({});
+  // const [queryValues, setQueryValues] = useState({});
+  const { mutate: fetchBooks, data: books } = useGetBooks();
 
   const handleSubmit = (values) => {
-    setQueryValues(values);
+    fetchBooks({
+      ...values,
+      category: values.category !== defaultCategory ? values.category : null,
+    });
   };
 
   const formik = useFormik({
@@ -27,7 +32,7 @@ const PageLayoutContainer = () => {
   });
 
   return (
-    <BooksListContext.Provider value={queryValues}>
+    <BooksListContext.Provider value={books}>
       <PageLayout formik={formik} />
     </BooksListContext.Provider>
   );
